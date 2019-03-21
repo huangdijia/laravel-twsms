47b31603564d9ab86dd82d0ace87b7ddd0223670
huangdijia <huangdijia@gmail.com>
2019-03-21 16:51:00 +0800

    add config

--- config/config.php
+++ config/config.php
@@ -1,2 +1,9 @@
 <?php
-return [];
+return [
+    'account'  => env('TWSMS_ACCOUNT', ),
+    'password' => env('TWSMS_PASSWORD', ),
+    'type'     => env('TWSMS_TYPE', 'now'),
+    'encoding' => env('TWSMS_ENCODING', 'big5'),
+    'vldtime'  => env('TWSMS_VLDTIME', ''),
+    'dlvtime'  => env('TWSMS_DLVTIME', ''),
+];
