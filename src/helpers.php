296deffadc115d6f7f64cd3b9ed829833276d7b5
huangdijia <huangdijia@gmail.com>
2019-09-20 16:49:33 +0800

    remove config_path()

--- src/helpers.php
+++ src/helpers.php
@@ -12,22 +12,3 @@ if (!function_exists('twsms_send')) {
         return app('sms.twsms')->send($mobile, $message) ?: app('sms.twsms')->getError();
     }
 }
-
-if (!function_exists('config_path')) {
-    /**
-     * Get the configuration path.
-     *
-     * @param  string  $name
-     * @return string
-     */
-    function config_path($name = '')
-    {
-        if (is_callable(app(), 'getConfigurationPath')) {
-            return app()->getConfigurationPath($name);
-        } elseif (app()->has('path.config')) {
-            return app()->make('path.config') . ($name ? DIRECTORY_SEPARATOR . $name : $name);
-        } else {
-            return app()->make('path') . '/../config' . ($name ? DIRECTORY_SEPARATOR . $name : $name);
-        }
-    }
-}
